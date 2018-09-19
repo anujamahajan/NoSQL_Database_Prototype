49bfad6793d5d527fdcebfe55992ec68bd4e06a5
anujamahajan <anuja.mahajan2210@gmail.com>
2018-09-19 01:58:36 -0400

    Add files via upload

---
+++ Test/Test.h
@@ -0,0 +1,45 @@
+#pragma once
+///////////////////////////////////////////////////////////////////////
+// Test.h - provides single-user test harness                        //
+// ver 1.0                                                           //
+// Language:    C++, Visual Studio 2017                              //
+// Application: Most Projects, CSE687 - Object Oriented Design       //
+// Author:      Anuja Mahajan, Syracuse University(MSCS-Spring 2018) //
+//				apmahaja@syr.edu									 //
+///////////////////////////////////////////////////////////////////////
+
+/*
+* Package Operations:
+* -------------------
+* This package defines test functions to demonstartes NoSQLDb prototype
+
+* Required Files:
+* ---------------
+* DateTime.h, DateTime.cpp
+*
+* Maintenance History:
+* --------------------
+* ver 1.0 : 10 Jan 2018
+* - first release
+*/
+
+//Defines test functions for requirements
+bool testR1();
+bool testR2();
+bool testR3a();
+bool testR3b();
+bool testR4b();
+bool testR5a();
+bool testR5b();
+bool testR5c();
+bool testR5d();
+bool testR6a();
+bool testR6b();
+bool testR6c();
+bool testR6d();
+bool testR6e();
+bool testR6f();
+bool testR7a();
+bool testR7b();
+bool testR8a();
+bool testR8b();
